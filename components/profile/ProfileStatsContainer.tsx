7a676f87d219a1fa54773622c40c25b275209f64
Theophilus <theophilusadesola002@gmail.com>
2025-12-10 11:56:01 +0100

    UI: Liquidity page stats

--- components/profile/ProfileStatsContainer.tsx
+++ components/profile/ProfileStatsContainer.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react"
 export default function ProfileStatsContainer({ children }: { children: ReactNode }) {
 	return (
 		<div className="w-full  backdrop-blur-sm relative z-10">
-			<div className="container mx-auto px-4 pb-1 pt-6 max-w-7xl">
+			<div className="pt-6">
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">{children}</div>
 			</div>
 		</div>
